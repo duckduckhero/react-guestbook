fadf24a0e11cd24107fcfb9908f2eb8e68d40461
Jeff Lee <jefflee.ldh@gmail.com>
2021-09-01 02:32:45 -0400

    everything except for decoration/css design

--- guestbook-frontend/src/components/MessageList.js
+++ guestbook-frontend/src/components/MessageList.js
@@ -1,46 +1,17 @@
 import React, { useState, useEffect } from 'react'; 
 import MessageItem from './MessageItem';
-import axios from 'axios';
 import { set } from 'harmony-reflect';
 
-function MessageList() {
-    const [messages, setMessages] = useState(null);
-    const [loading, setLoading] = useState(null);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-
-        const getMessages = async () => {
-            try{
-            
-                setError(null);
-                setMessages(null);
-
-                setLoading(true);
-                const response = await axios.get('http://localhost:80/message/read');
-                setMessages(response.data);
-
-            } catch(e) {
-                console.log("에러 발생 : " +e);
-                setError(e);
-            }
-            setLoading(false);
-        };
-
-        getMessages();
-
-    }, []);
-
+function MessageList({loading, error, messages}) {
+    
     if(loading) return(<div>로딩 중...</div>);
     if(error) return(<div>에러 발생...</div>);
     if(!messages) return(null); 
     return(
         <div>
-
             {messages.data.map(message => (
                 <MessageItem username={message.username} message={message.message} date={message.date} key={message.message_id}/>
             ))}
-
         </div>
     )
 }
--- guestbook-frontend/src/components/Messageboard.js
+++ guestbook-frontend/src/components/Messageboard.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './Messageboard.css'
 import MessageList from './MessageList';
+import axios from 'axios';
 
 function Messageboard() {
+
+    const [messages, setMessages] = useState(null);
+    const [loading, setLoading] = useState(null); 
+    const [error, setError] = useState(null);
+
+    const nameInput = useRef();
+    const messageInput = useRef(); 
+
+   
+
+    //메시지 불러오기 위한 함수 
+    const getMessages = async () => {
+        try{
+        
+            setError(null);
+            setMessages(null);
+
+            setLoading(true);
+            const response = await axios.get('http://localhost:80/message/read');
+            setMessages(response.data);
+
+
+        } catch(e) {
+            console.log("에러 발생 : " +e);
+            setError(e);
+        }
+        setLoading(false);
+    };
+
+    //submit 버튼 눌렸을 때 일어나는 것들 처리 
+    const addMessages = async () => {
+        console.log("버튼이 눌렸습니다");
+
+        
+        axios.post('http://localhost:80/message/add', {
+            username : nameInput.current.value, 
+            message : messageInput.current.value
+        }).then((response) => { //정상적으로 메시지 등록이 되었을 때 
+            console.log(response);
+            getMessages();
+        }).catch((error) => {
+            console.log(error);
+        })
+        
+
+        
+    }
+
+    useEffect(() => {
+        getMessages();
+    }, []);
+
+    
     return(
         <div className="messageBoard">
             <div className="messageBox">  
-                <input placeholder="name"/>
-                <input placeholder="message"/>
-                <button>submit</button>
+                <input placeholder="name" ref={nameInput}/>
+                <input placeholder="message" ref={messageInput}/>
+                <button onClick={addMessages}>submit</button>
             </div>
-            <MessageList />
+            <MessageList error={error} loading={loading} messages={messages}/>
         </div>
     );
 }
